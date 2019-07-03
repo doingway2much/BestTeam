e6ca5383c604b2305234d96e1e9c1313e50d3ebe
Cloud <claudiojsaillant@gmail.com>
2019-07-02 20:39:31 -0400

    adding jquery

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $.ajax({
     url: "https://api.nytimes.com/svc/archive/v1/2019/1.json?api-key=AURIva3W9i7nNcn9aJo6G5jme8AxjZqn",
-    method: "GET";
+    method: "GET"
 
 }).then(function(data){
     console.log(data);
